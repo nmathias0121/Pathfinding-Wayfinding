52215e1a1c00ce3e1a4feec52becea582754ada2
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-10-11 20:17:29 -0700

    add completed project files

---
+++ Wayfinding/Assets/FollowWP.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWP : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int currentWP = 0;
+
+    public float speed = 7.0f;
+    public float rotSpeed = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+        {
+            currentWP = waypoints.Length - 1;
+            return;
+        }
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
